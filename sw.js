e822a2237c2e0ef58d751a2c7e783f91a68ad0f2
cjwandjy <cjwandjy@gmail.com>
2026-06-13 07:47:42 +0000

    Add condition boosters and battle arenas

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "poop-home-run-v2";
+const CACHE = "poop-home-run-v3";
 const ASSETS = ["./", "./index.html", "./style.css", "./main.js", "./manifest.webmanifest", "./icon.svg"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
