796fa5aca1d88c7e0079cf646a8c7d774fd39d0d
cjwandjy <cjwandjy@gmail.com>
2026-06-16 05:05:24 +0000

    Fix gameplay progression and weapon timing

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "poop-home-run-v10";
+const CACHE = "poop-home-run-v12";
 const ASSETS = [
   "./", "./index.html", "./style.css", "./main.js", "./manifest.webmanifest", "./icon.svg",
   "./UI/main-page.png", "./UI/tool.png", "./UI/boss.png", "./UI/faces.png"
